51756c37ba98f8e90d40ccfe5a3017e5d6a5efa2
Chirag Raju <chiragrajus2002@gmail.com>
2026-02-11 00:36:03 +0530

    COMMIT-1 User Module Auth Design

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+import { User } from './users/users.entity';
+import { Report } from './reports/report.entity';
+
+@Module({
+  imports: [
+    UsersModule,
+    ReportsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [User, Report],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/users/users.entity.ts
@@ -0,0 +1,34 @@
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  email: string;
+  @Column()
+  password: string;
+
+  //these are lifecycle hooks that will be called after the corresponding events occur on the entity. They are useful for logging, auditing, or performing additional actions when an entity is inserted, updated, or removed from the database.
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted user with id:', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated user with id:', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed user with id:', this.id);
+  }
+}
