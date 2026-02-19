63ff64c798c50b3feeef3086b6f2e10f6204db13
Chirag Raju <chiragrajus2002@gmail.com>
2026-02-20 00:25:11 +0530

    COMMIT-4 Serializtion Interceptors (Custom Data DTO Mapping)

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,24 +1,30 @@
 import {
-    UseInterceptors,
-    NestInterceptor,
-    ExecutionContext,
-    CallHandler,
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
 
-
-export class SerializeInterceptor implements NestInterceptor {
-    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-        //Run Something before a request is handled by the request handler
-        console.log('I am Running before Handler', context);
-        return next.handle().pipe(map((data: any) => {
-            //Run Something before the response is sent out
-            console.log('I am Running before Response is Sent Out', data);
-        
-         }));
-    }
+interface ClassConstructor {
+  new (...args: any[]): {};
 }
 
+export function Serialize(dto: any) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
 
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    //Run Something before a request is handled by the request handler
+    console.log('I am Running before Handler', context);
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToClass(this.dto, data, { excludeExtraneousValues: true });
+      }),
+    );
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,12 +13,16 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user-dto';
-import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
+
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  @UseInterceptors(SerializeInterceptor)
+  //@UseInterceptors(new SerializeInterceptor(UserDto))
+  //@Serialize(UserDto)
   @Get('/:id')
   findUser(@Param('id') id: string) {
     console.log('I am Running before Handler', id);
